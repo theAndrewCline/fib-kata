107acb6a06f7f6e0bad8000ea6a6268a28534337
pavansai003 <pavansai15@iiitkottayam.ac.in>
2020-07-22 19:42:10 +0530

    test completed

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 function main(num) {
-  return undefined
-}
 
+  function fibonacci_series(n) {
+    if (n === 1) {
+      return [0, 1];
+    }
+    else {
+      var s = fibonacci_series(n - 1);
+      s.push(s[s.length - 1] + s[s.length - 2]);
+      return s;
+    }
+
+
+  };
+
+
+  var fibarray = fibonacci_series(100);
+  if (num < 0) {
+    console.log("Number cant be negative");
+  }
+  else {
+    
+    return fibarray[num]; 
+  }
+
+
+}
+main(5);
 module.exports = main
