9ba9785064fb504bd0a4c79c29f789b1187f76c1
naveenkumar456 <68639076+naveenkumar456@users.noreply.github.com>
2020-07-22 18:07:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function main(num) {
-  return undefined
+        if(num < 0) throw new Error('num cannot be negative number')  
+        else if(num === 0) return 0;
+        return (num === 1 || num === 2) ? num-1 : main(num-1) + main(num-2)
+    }
 }
 
 module.exports = main
